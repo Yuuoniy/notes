8216c39548f2f2342bd4bac2fac94f56b346b75c
yuuoniy <linmq006@gmail.com>
2018-12-18 11:38:07 +0800

    fix sec problms

--- node.js/node.js实战/代码/public/javascripts/chat_ui.js
+++
@@ -1,7 +0,0 @@
-function divEscapedContentElement(message) {
-  return $('<div></div>').text(message);
-}
-
-function divSystemContentElement(message) {
-  return $('<div></div>').html('<i>' + message + '</i>');
-}
